6f9326850b492a20672d32c7d2b906434a8cba61
Francososa <franky.sosa@gmail.com>
2022-03-23 21:29:22 -0600

    fix data types

--- src/fkv_mem.rs
+++ src/fkv_mem.rs
@@ -24,25 +24,26 @@ fn main() {
     let fname = args.get(1).expect(&USAGE);
     let action: &[u8] = args.get(2).expect(&USAGE).as_ref();
     let key: &[u8] = args.get(3).expect(&USAGE).as_ref();
+    let maybe_value = args.get(4);
 
     let path = std::path::Path::new(&fname);
     let mut store = FranKV::open(path).expect("unable to open file");
     store.load().expect("unable to load data");
 
     match action {
-        "get" => match store.get(key).unwrap {
+        b"get" => match store.get(key).unwrap() {
             None => eprintln!("{:?} not found", key),
             Some(value) => println!("{:?}", value),
         },
 
-        "delete" => store.delete(key).unwrap(),
+        b"delete" => store.delete(key).unwrap(),
 
-        "insert" => {
+        b"insert" => {
             let value = maybe_value.expect(&USAGE).as_ref();
             store.insert(key, value).unwrap()
         },
 
-        "update" => {
+        b"update" => {
             let value = maybe_value.expect(&USAGE).as_ref();
             store.update(key, value).unwrap()
         },
